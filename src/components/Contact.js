70fabf9ed792641054e859bbfb1a921ad11d7941
adlihardiyanto <104336544+adlihardiyanto@users.noreply.github.com>
2022-10-12 17:33:43 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,33 @@
 // Terima props lalu tampilkan dalam Contact component
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
-import { Divider, Typography } from '@mui/material';
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
 import React from 'react';
-
+import { indigo } from "@mui/material/colors";
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    const { photo, name, phone, email } = data
-    return (
-    <>
-        <div style={{ display: 'flex', alignItem: 'center', marginBottom: '10px' }}>
-            <img src={ photo } alt='gambar' width={80} height={80} style={{ borderRadius: '50%', marginRight:'40px' }} />
-            <div style={{ textAlign:'left' }}>
-                <Typography variant='body1' component='div'>
-                    { name }
-                </Typography>
-                <Typography variant='subtitle1' component='div' color='text.secondary'>
-                    { phone }
-                </Typography>
-                <Typography variant='subtitle1' component='div' color='text.secondary'>
-                    { email }
-                </Typography>
-            </div>
-            <Divider style={{ color: 'blue' }} />
-        </div>
-        <Divider style={{ marginBottom: '20px' }} />
-    </>
-    )
-}
+    return (<>
+    <List sx={{bgcolor: indigo[100]}}>
+   <ListItem alignItems="flex-start">
+    <ListItemAvatar>
+        <Avatar src={data.photo}></Avatar>
+    </ListItemAvatar>
+    <ListItemText
+				primary={data.name}
+				secondaryTypographyProps={{ component: "div" }}
+				secondary={
+					<React.Fragment>
+						<Typography>{data.phone}</Typography>
+						<Typography>{data.email}</Typography>
+					</React.Fragment>
+				}
+				title={data.name}
+			/>
+   </ListItem>
+   </List>
+    </>);
+};
 
 export default Contact;
