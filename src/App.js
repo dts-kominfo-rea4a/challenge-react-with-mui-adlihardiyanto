a2d2e04142389eb592e2f0248cc1818a762c8326
adlihardiyanto <104336544+adlihardiyanto@users.noreply.github.com>
2022-10-12 17:26:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import { Card, CardContent, Grid } from '@mui/material';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 import './App.css';
-
-import Contact from './components/Contact';
-import ContactForm from './components/ContactForm';
-import Header from './components/Header';
+import Header from "./components/Header";
+import { Grid, Divider,  List, Paper} from "@mui/material";
+import ContactForm from "./components/ContactForm";
+import Contact from "./components/Contact";
+import { grey,indigo } from "@mui/material/colors";
 
 // Uncomment untuk memuat daftar kontak
 import contactsJSON from './data/contacts.json';
-
 const App = () => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
   // https://mui.com/material-ui/react-list/#folder-list
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
-  const [contacts, setContact] = useState(contactsJSON)
-
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
-  const addContact = (v) => {
-    console.log('value', v)
-    setContact([...contacts, v])
-  }
+  const [contacts, setContacts] = useState([contactsJSON]);
+
+  useEffect(() => {
+    setContacts(contactsJSON);
+  }, []);
+
+  const addContact = (contact) => {
+    setContacts([...contacts, contact]);
+  };
 
   return (
     <div className="App">
-      <Header />
-      <Grid container style={{ padding: '40px 0' }}>
-        <Grid item md={6}>
-            <ContactForm fnAddContact={(v) => addContact(v)} />
+      <Header/>
+      <Grid container spacing ={7} padding={5}>
+        <Grid item xs={12} md={6}>
+          <ContactForm handleSave={addContact}/>
         </Grid>
-        <Grid item md={6}>
-          <Card sx={{ maxWidth: 600, backgroundColor: '#C6EBC5' }}>
-            <CardContent >
-                  {contacts.map((contact, index) => (
-                    <Contact key={index} data={contact} />
-                  ))}
-            </CardContent>
-          </Card>
-        </Grid>
+        <Grid item xs={12} md={6}>
+					<Paper variant="outlined" sx={{ bgcolor: grey[50] }}>
+          <List sx={{bgcolor: indigo[50]}}>
+							{contacts.map((contact, index) => (
+								<div key={index}>
+									<Contact data={contact} />
+									{index !== contacts.length - 1 && <Divider />}
+								</div>
+							))}
+						</List>
+					</Paper>
+				</Grid>
       </Grid>
     </div>
   );
